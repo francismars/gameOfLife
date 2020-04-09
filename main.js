a39fa7b6109060ace00b34541a7f3fdc385c5616
Francisco <11526661+francismars@users.noreply.github.com>
2020-04-09 19:21:46 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,101 @@
 const cellSize = 10
 const DEAD = 0
 const ALIVE = 1
+let range=cellSize/2
 let cellGrid = []
 let gridClone = []
 let cellNeighbours = []
+let pause = 0
+let generation = 0
 
 // Cell Object
-function Cell(x,y,alive) {
+function Cell(x,y,living) {
 		this.x = x
 		this.y = y
-		this.alive = alive		
+		this.living = living		
 		this.draw = function(){
-			stroke(0)
-			alive ? fill(255) : fill(0)			
-			square(this.x,this.y,cellSize,cellSize)
+			// On Mouse Over
+			if (i*cellSize + range > (mouseX - range) && i*cellSize + range < (mouseX + range) && 
+				j*cellSize + range > (mouseY - range) && j*cellSize + range < (mouseY + range)) {
+				stroke(255)
+				cellGrid[i][j].living ? fill(123) : fill(123)			
+				square(i*cellSize,j*cellSize,cellSize,cellSize)
+			}
+			else {
+				stroke(0)
+				this.living ? fill(255) : fill(0)			
+				square(this.x,this.y,cellSize,cellSize)
+			}
+		}
+		this.flip = function() {
+			this.living ? this.living=DEAD : this.living=ALIVE
 		}
 }
 
-function setup() {
-	frameRate(24)
-	createCanvas(640, 480);
+//Creates New Empty Grid
+function initiateGrid() {
 	for(i=0;i<width/cellSize;i++){
 		cellGrid[i] = []
 		for(j=0;j<height/cellSize;j++){
-			alive = Math.floor(Math.random() * 2)
-			cellGrid[i][j] = new Cell(i*cellSize,j*cellSize,alive)
+			living = Math.floor(Math.random() * 2)
+			cellGrid[i][j] = new Cell(i*cellSize,j*cellSize,living)
 		}
+	}	
+}
+
+function setup() {
+	createCanvas(640, 480)
+	initiateGrid()	
+	
+	textGeneration = createP()
+	textGeneration.position(700, 75)
+	
+	buttonPause = createButton("Pause")
+	buttonPause.position(700, 125)
+	buttonPause.mousePressed(pauseGame)
+	
+	buttonReset = createButton("Reset")
+	buttonReset.position(700, 150)
+	buttonReset.mousePressed(resetGame)
+}
+
+
+function resetGame(){
+	for(i=0;i<width/cellSize;i++){
+		for(j=0;j<height/cellSize;j++){				
+			cellGrid[i][j].living=0
+		}
+	}	
+	pauseGame()	
+}
+
+function mouseClicked() {
+	if(mouseX<width && mouseY<height){
+		cellGrid[Math.floor(mouseX/cellSize)][Math.floor(mouseY/cellSize)].flip()		
 	}
 }
 
-function draw() {
-	if(gridClone[0]){
-		cellGrid = gridClone
-		gridClone = []
+function pauseGame(){
+	if(pause==0){
+		buttonPause.html("Play")
+		pause=1
+		//noLoop()
+		
+	} else {
+		buttonPause.html("Pause")
+		pause=0
+		//loop()
+		
 	}
-	for(i=0;i<width/cellSize;i++){
-		for(j=0;j<height/cellSize;j++){
-			cellGrid[i][j].draw()
-		}
-	}
+}
 
-	for(i=0;i<width/cellSize;i++){
-		cellNeighbours[i] = []
-		for(j=0;j<height/cellSize;j++){
-			cellNeighbours[i][j] = 0
-			for(k=-1;k<=1;k++){
-				for(l=-1;l<=1;l++){
-					cellNeighbours[i][j] += cellGrid[((i-k)+cellGrid.length) % cellGrid.length][((j-l)+cellGrid[0].length) % cellGrid[0].length].alive
-				}
-			}
-			cellNeighbours[i][j] -= cellGrid[i][j].alive
-		}
-	}	
-	
+// Creates Next Generation
+function newGeneration() {
 	for(i=0;i<width/cellSize;i++){
 		gridClone[i] = []
+		// Rules Of The Game
 		for(j=0;j<height/cellSize;j++){
-			if(cellGrid[i][j].alive==ALIVE){
+			if(cellGrid[i][j].living==ALIVE){
 				if(cellNeighbours[i][j]<2 || cellNeighbours[i][j]>3){
 					gridClone[i][j] = new Cell(i*cellSize,j*cellSize,DEAD)
 				}
@@ -64,7 +103,7 @@ function draw() {
 					gridClone[i][j] = new Cell(i*cellSize,j*cellSize,ALIVE)
 				}
 			}
-			else if(cellGrid[i][j].alive==DEAD){
+			else if(cellGrid[i][j].living==DEAD){
 				if(cellNeighbours[i][j]==3){
 					gridClone[i][j] = new Cell(i*cellSize,j*cellSize,ALIVE)
 				}
@@ -74,4 +113,40 @@ function draw() {
 			}
 		}
 	}
+	generation++
+	textGeneration.html('Generation: '+generation)
+	cellGrid = gridClone
+	gridClone = []
+}
+
+// Check Number of Neighbours per Cell
+function checkNeighbours() {	
+	for(i=0;i<width/cellSize;i++){
+		cellNeighbours[i] = []
+		for(j=0;j<height/cellSize;j++){
+			cellNeighbours[i][j] = 0
+			for(k=-1;k<=1;k++){
+				for(l=-1;l<=1;l++){
+					neighbourX = ((i-k)+cellGrid.length) % cellGrid.length
+					neighbourY = ((j-l)+cellGrid[0].length) % cellGrid[0].length
+					cellNeighbours[i][j] += cellGrid[neighbourX][neighbourY].living
+				}
+			}
+			cellNeighbours[i][j] -= cellGrid[i][j].living
+		}
+	}
+}
+
+function draw() {
+	// Draw all Cells
+	for(i=0;i<width/cellSize;i++){
+		for(j=0;j<height/cellSize;j++){
+			cellGrid[i][j].draw()			
+		}
+	}
+	if(!pause){
+		checkNeighbours()	
+		newGeneration()		
+	}
+
 }
